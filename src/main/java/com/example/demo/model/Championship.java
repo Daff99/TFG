f0dc15d45fbe0383bb822e8ca3c3e85be0361ff3
Daff99 <davidnavarro278@gmail.com>
2024-09-19 13:26:55 +0200

    comienza a funcionar la base de datos

--- src/main/java/com/example/demo/model/Championship.java
+++ src/main/java/com/example/demo/model/Championship.java
@@ -1,5 +1,32 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name ="CHAMPIONSHIPS")
+@Data
 public class Championship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "IDCHAMPIONSHIP")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "IDUSER")
+    @JsonIgnore
+    private User user;
+    
     
 }
--- src/main/java/com/example/demo/model/Player.java
+++ src/main/java/com/example/demo/model/Player.java
@@ -1,5 +1,28 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Table(name ="PLAYERS")
+@Data
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "IDPLAYER")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "IDUSER")
+    @JsonIgnore
+    private User user;
     
 }
